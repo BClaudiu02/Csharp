cb3f77e3bc3f8dde7d96a3631adaca1a97ea1070
StudentClaudiu <bogdanc89@yahoo.de>
2026-02-23 16:29:17 +0200

    exercises with numbers and strings

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,48 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 23;
+            Console.WriteLine(age);
+
+            age++;
+            Console.WriteLine(age);
+            Console.WriteLine(++age);
+
+            long bigNumber = 9000000L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+
+            double negative = -55.2;
+            Console.WriteLine(negative);
+
+            string textPrecision = "35.140001";
+            float precision = Convert.ToSingle(textPrecision);
+            Console.WriteLine(precision);
+
+            string textMoney = "100.54";
+            decimal money = Convert.ToDecimal(textMoney);
+            Console.WriteLine(money);
+
+            string name = "John Doe";
+            Console.Write("Your name is: ");
+            name += " and you are a student.";
+            Console.Write(name);
+            Console.WriteLine();
+
+            char letter = 'A';
+            Console.WriteLine(letter);
+
+
+            bool value = true;
+            Console.WriteLine(value);
+
+            value = false;
+            Console.WriteLine(value);
+
+            
+
+            Console.ReadLine();
         }
     }
 }
