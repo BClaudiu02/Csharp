ec10fd4c488a0d91da06f72e156f931fc14fffcb
StudentClaudiu <bogdanc89@yahoo.de>
2026-03-19 16:14:31 +0200

    added Create Prize Form

--- TournamentTracker/TrackerUI/CreateTeamForm.Designer.cs
+++ TournamentTracker/TrackerUI/CreateTeamForm.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.teamNameValue = new System.Windows.Forms.TextBox();
             this.teamNameLabel = new System.Windows.Forms.Label();
-            this.createTeamLabel = new System.Windows.Forms.Label();
+            this.headerLabel = new System.Windows.Forms.Label();
             this.addMemberButton = new System.Windows.Forms.Button();
             this.selectTeamMemberDropDown = new System.Windows.Forms.ComboBox();
             this.selectTeamMemberLabel = new System.Windows.Forms.Label();
@@ -71,17 +71,17 @@
             this.teamNameLabel.Text = "Team Name";
             this.teamNameLabel.Click += new System.EventHandler(this.teamNameLabel_Click);
             // 
-            // createTeamLabel
+            // headerLabel
             // 
-            this.createTeamLabel.AutoSize = true;
-            this.createTeamLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.createTeamLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-            this.createTeamLabel.Location = new System.Drawing.Point(29, 31);
-            this.createTeamLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.createTeamLabel.Name = "createTeamLabel";
-            this.createTeamLabel.Size = new System.Drawing.Size(213, 50);
-            this.createTeamLabel.TabIndex = 11;
-            this.createTeamLabel.Text = "Create Team";
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.headerLabel.Location = new System.Drawing.Point(29, 31);
+            this.headerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(213, 50);
+            this.headerLabel.TabIndex = 11;
+            this.headerLabel.Text = "Create Team";
             // 
             // addMemberButton
             // 
@@ -294,7 +294,7 @@
             this.Controls.Add(this.selectTeamMemberLabel);
             this.Controls.Add(this.teamNameValue);
             this.Controls.Add(this.teamNameLabel);
-            this.Controls.Add(this.createTeamLabel);
+            this.Controls.Add(this.headerLabel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "CreateTeamForm";
@@ -310,7 +310,7 @@
 
         private System.Windows.Forms.TextBox teamNameValue;
         private System.Windows.Forms.Label teamNameLabel;
-        private System.Windows.Forms.Label createTeamLabel;
+        private System.Windows.Forms.Label headerLabel;
         private System.Windows.Forms.Button addMemberButton;
         private System.Windows.Forms.ComboBox selectTeamMemberDropDown;
         private System.Windows.Forms.Label selectTeamMemberLabel;
